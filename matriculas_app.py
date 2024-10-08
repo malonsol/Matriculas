3ff1497ee29f70323baaaff2720aedb5c9bd7d3a
malonsol <71399207+malonsol@users.noreply.github.com>
2024-10-08 18:21:25 +0100

    Update matriculas_app.py
    Cambio de ruta local a ruta en GitHub.

--- matriculas_app.py
+++ matriculas_app.py
@@ -1,9 +1,12 @@
 import streamlit as st
+import os
 
 # Cargar el diccionario de palabras
 @st.cache
 def load_dictionary():
-    with open(r'C:\Users\turge\Desktop\Python\Projects\Matriculas\dic_rae.txt', 'r', encoding='utf-8') as file:
+    # Obtener la ruta del archivo dic_rae.txt relativa al script
+    dictionary_path = os.path.join(os.path.dirname(__file__), 'dic_rae.txt')
+    with open(dictionary_path, 'r', encoding='utf-8') as file:
         words = file.read().splitlines()
     return words
 
@@ -38,20 +41,4 @@ consonants_input = st.text_input('Consonantes (3):', max_chars=3)
 # Procesar la entrada del usuario
 if consonants_input:
     # Filtrar la entrada para quedarnos solo con consonantes
-    consonants_filtered = [c for c in consonants_input if is_consonant(c)]
-
-    if len(consonants_filtered) == 3:
-        words = load_dictionary()
-        matching_words = find_words_with_consonants(consonants_filtered, words)
-
-        # Mostrar resultados
-        if matching_words:
-            st.write('Palabras encontradas:')
-            for word in matching_words:
-                st.write(word)
-        else:
-            st.write('No se encontraron palabras que contengan esas consonantes.')
-    else:
-        st.write('Por favor, introduce exactamente 3 consonantes. Cualquier otro carácter (número, vocales, símbolos...) será omitido.')
-else:
-    st.write('Por favor, introduce exactamente 3 consonantes.')
+    consonants_filtered = [c for c in
