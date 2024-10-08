036e1ccb24282ac209dc563e654ab6d999f166ba
malonsol <71399207+malonsol@users.noreply.github.com>
2024-10-08 18:32:28 +0100

    Update matriculas_app.py
    Coche 3

--- matriculas_app.py
+++ matriculas_app.py
@@ -5,7 +5,7 @@ import os
 st.markdown("""
 <style>
     body {
-        background-image: url('https://unsplash.com/es/fotos/coupe-descapotable-aston-martin-negro-aparcado-junto-a-un-puente-de-hormigon-azul-x8l63IUeuwQ'); /* URL de la imagen */
+        background-image: url('https://images.unsplash.com/photo-1535732820275-9ffd998cac22?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'); /* URL de la imagen */
         background-size: cover;
         background-position: center;
         color: white; /* Cambiar color del texto si es necesario */
