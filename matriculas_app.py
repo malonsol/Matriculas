ff7fbcade374f69982a36ca77e39714cd2ec9f77
malonsol <71399207+malonsol@users.noreply.github.com>
2024-10-08 18:50:01 +0100

    Update matriculas_app.py
    coche repo

--- matriculas_app.py
+++ matriculas_app.py
@@ -1,28 +1,31 @@
 import streamlit as st
 import os
 
+# URL de la imagen en GitHub
+image_url = 'https://raw.githubusercontent.com/tu_usuario/tu_repositorio/rama/coche.jpg'  # Cambia esto por tu URL
+
 # Estilo CSS para la aplicación
-st.markdown("""
+st.markdown(f"""
 <style>
-    .main {
-        background-image: url('https://images.unsplash.com/photo-1517942228356-63411b895c49?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDF8fGNhdHxlbnwwfHx8fDE2MzY4OTg4MjE&ixlib=rb-1.2.1&q=80&w=1080'); /* URL de la imagen */
+    .main {{
+        background-image: url('{image_url}'); /* URL de la imagen */
         background-size: cover;
         background-position: center;
         color: white; /* Cambiar color del texto si es necesario */
-    }
-    h1 {
+    }}
+    h1 {{
         color: #4CAF50; /* Color del título */
         font-family: 'Poppins', sans-serif; /* Fuente del título */
-    }
-    h2 {
+    }}
+    h2 {{
         color: #FF9800; /* Color del subtítulo */
         font-family: 'Poppins', sans-serif; /* Fuente del subtítulo */
-    }
-    .result {
+    }}
+    .result {{
         font-family: 'Roboto', sans-serif; /* Fuente del texto de resultados */
         color: #212121; /* Color del texto */
         margin: 10px 0;
-    }
+    }}
 </style>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400&display=swap" rel="stylesheet">
 """, unsafe_allow_html=True)
